2cc811b52614af03073d44bed6e9cac29379c642
Tarakpatil <patiltarak04@gmail.com>
2025-04-27 23:19:31 +0530

    changes in slider code

--- main.js
+++ main.js
@@ -79,8 +79,16 @@ ScrollReveal().reveal(".chef__list li", {
 
 const swiper = new Swiper(".swiper", {
   loop: true,
-
   pagination: {
     el: ".swiper-pagination",
+    clickable: true, 
+  },
+  navigation: {
+    nextEl: ".swiper-button-next", 
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000, 
+    disableOnInteraction: false, 
   },
 });
